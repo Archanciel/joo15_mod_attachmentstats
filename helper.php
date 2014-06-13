3dca2c26c9028f5853c21912bb2779c0cee05c89
Archanciel <jp.schnyder@gmail.com>
2014-06-13 17:50:45 +0200

    modif to use ignore_count & ignore_length
    ignore_count & ignore_length are in fact user_field_1 & user_field_2 in
    jos_attachments.

--- helper.php
+++ helper.php
@@ -14,7 +14,8 @@ require_once (MOD_ATTACHMENTSTATS_BASE . DS . 'constants.php');
 class modAttachmentStatsHelper {
 	private static $instance;
 	
-	private $statsQuery;
+	private $countQuery;
+	private $lengthAndSizeQuery;
 	
 	public static function getInstance() {
 		if (!isset(self::$instance)) {
@@ -34,7 +35,26 @@ class modAttachmentStatsHelper {
     	/* @var $db JDatabase */
     	$db = JFactory::getDBO();
     	
-    	$db->setQuery($this->statsQuery);
+    	$db->setQuery($this->countQuery);
+    	$assoc = $db->loadAssoc();
+    	
+    	if( $db->getErrorNum () ) {
+			$e = $db->getErrorMsg();
+			//print_r( $e );
+			JError::raiseError( 500, $e );
+			return;
+    	}
+    	
+    	$isDataCollected = TRUE;
+    	
+    	if ($assoc) {
+    		$recordingNb = number_format($assoc["R_COUNT"],0,',',' ');
+    		$downloadCount = number_format($assoc["DL_COUNT"],0,',',' ');
+    	} else {
+    		$isDataCollected = FALSE;
+    	}
+    	
+    	$db->setQuery($this->lengthAndSizeQuery);
     	$assoc = $db->loadAssoc();
     	
     	if( $db->getErrorNum () ) {
@@ -45,22 +65,46 @@ class modAttachmentStatsHelper {
     	}
     	
     	if ($assoc) {
+    		$totalSize = number_format($assoc["TOT_SIZE"],0,',',' ').' MB';
     		$timeFloat = $assoc["TOT_TIME"];
     		$timeHH = (int)$timeFloat;
     		$timeMM = (int)(($timeFloat - $timeHH) * 60);
     		$timeStr = $timeHH . ' H ' . $timeMM . "'";
-        	return array(REC_COUNT_POS => number_format($assoc["R_COUNT"],0,',',' '),REC_TOTAL_SIZE_POS => number_format($assoc["TOT_SIZE"],0,',',' ').' MB',LISTENING_TOTAL_TIME_POS => $timeStr,DOWNLOAD_COUNT_POS => number_format($assoc["DL_COUNT"],0,',',' '));
     	} else {
-        	return array(REC_COUNT_POS => 0,REC_TOTAL_SIZE_POS => 0,DOWNLOAD_COUNT_POS => 0,LISTENING_TOTAL_TIME_POS => 0);
+    		$isDataCollected = FALSE;
     	}
+    	
+    	if ($isDataCollected) {
+    		return array(REC_COUNT_POS => $recordingNb,REC_TOTAL_SIZE_POS => $totalSize,LISTENING_TOTAL_TIME_POS => $timeStr,DOWNLOAD_COUNT_POS => $downloadCount);
+       	} else {
+       		return array(REC_COUNT_POS => 0,REC_TOTAL_SIZE_POS => 0,DOWNLOAD_COUNT_POS => 0,LISTENING_TOTAL_TIME_POS => 0);
+       	}
      }
 	
+     /**
+      * user_field_2 == ignore_count:	if value is 1, means do not include this line for computing the 
+      * 								total number of recordings.
+      * user_field_3 == ignore_length:	if value is 1, means do not include this line for computing the
+      * 								total recording length and size.
+      * 
+      * Example:
+      * 
+      * 													ignore_count	ignore_length
+      * 
+      * Malvoyant, et exceptionnel: partial attachment 1		empty			empty
+      * 							partial attachment 2		  1				empty
+      * 
+      * Le pouvoir du moment prés:	full attachment				empty			empty
+      * 							partial attachment 1		  1				  1
+      * 							partial attachment n		  1				  1
+      */
      private function __construct() {
-     	$this->statsQuery = "SELECT COUNT(filename) AS R_COUNT".
-		    			 ", SUM(user_field_1) / 60 AS TOT_TIME".
-		    			 ", SUM(download_count) AS DL_COUNT".
-		    			 ", SUM(file_size) / 1000000 AS TOT_SIZE".
-		    			 " FROM #__attachments;";
+     	$this->countQuery = "SELECT COUNT(filename) AS R_COUNT, SUM(download_count) AS DL_COUNT   
+							 FROM #__attachments AS a 
+     						 WHERE a.user_field_2 = '';";
+     	$this->lengthAndSizeQuery = "SELECT SUM(user_field_1) / 60 AS TOT_TIME, SUM(file_size) / 1000000 AS TOT_SIZE 
+							 		FROM #__attachments AS a 
+     						 		WHERE a.user_field_3 = '';";
      }
 }
 ?>
